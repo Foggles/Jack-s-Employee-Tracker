083c7b34f7f07eee0e1d6a6cb879e425a7e3cd3a
Jack Fogarty <jackfogarty2105@gmail.com>
2021-02-18 18:00:08 +0800

    Set up if & else if statements depending on which option the user selects

--- index.js
+++ index.js
@@ -28,4 +28,34 @@ inquirer
         }
     ]).then((response) => {
         console.log(`You selected to ${response.initialChoice}`);
+
+        if(response.initialChoice === "Add Employee") {
+            console.log(1);
+
+        }
+        else if(response.initialChoice === "Add Department") {
+            console.log(2);
+
+        }
+        else if(response.initialChoice === "Add Role") {
+            console.log(3);
+
+        }
+        else if(response.initialChoice === "View Employees") {
+            console.log(4);
+
+        }
+        else if(response.initialChoice === "View Departments") {
+            console.log(5);
+
+        }
+        else if(response.initialChoice === "View Roles") {
+            console.log(6);
+
+        }
+        else if(response.initialChoice === "Update Employee Roles") {
+            console.log(7);
+
+        }
+
     });

4925da25ffb43b79c4ac97b0f892ca870ba0763e
Jack Fogarty <jackfogarty2105@gmail.com>
2021-02-18 17:55:45 +0800

    Defined connection and created the initial user prompt with inquirer

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+
+const connectionConfig = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    passwoord: "12345",
+    database: "employees_db"
+};
+
+const connection = mysql.createConnection(connectionConfig);
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "initialChoice",
+            choices: [
+                "Add Employee",
+                "Add Department",
+                "Add Role",
+                "View Employees",
+                "View Departments",
+                "View Roles",
+                "Update Employee Roles"
+            ],
+        }
+    ]).then((response) => {
+        console.log(`You selected to ${response.initialChoice}`);
+    });

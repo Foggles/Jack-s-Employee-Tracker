c2b462409fe506da3ea0d35f2fd540be7d21195c
Jack Fogarty <jackfogarty2105@gmail.com>
2021-02-20 14:52:39 +0800

    User can now add & view employees, departments and roles - along with exiting the menu

--- index.js
+++ index.js
@@ -11,164 +11,214 @@ const connectionConfig = {
 
 const connection = mysql.createConnection(connectionConfig);
 
-inquirer
-    .prompt([
-        {
-            type: "list",
-            name: "initialChoice",
-            choices: [
-                "Add Employee",
-                "Add Department",
-                "Add Role",
-                "View Employees",
-                "View Departments",
-                "View Roles",
-                "Update Employee Roles"
-            ],
-        }
-    ]).then((response) => {
-        console.log(`You selected to ${response.initialChoice}`);
-
-        connection.connect((connectionError) => {
-            if (connectionError) {
-                throw connectionError;
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "first_name",
+                message: "What is the employee's first name? (30 Characters)"
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is the employee's last name? (30 Characters)"
+            },
+            {
+                type: "input",
+                name: "role_id",
+                message: "What is the employee's role id?"
+            },
+            {
+                type: "input",
+                name: "manager_id",
+                message: "What is the employee's manager's id? Can be left blank if no manager."
             }
-            else if (response.initialChoice === "Add Employee") {
+        ]).then((response) => {
+            const asd = Object.keys(response).filter(k => response[k] !== '').reduce((res, key) => {
+                res[key] = response[key];
+                return res;
+            }, {});
+
+            console.log('AAD', asd)
+
+            connection.query("INSERT INTO employee SET ?", asd, (queryErr, queryResponse) => {
+                if (queryErr) {
+                    throw queryErr;
+                }
+                console.log("Success");
+                startMenu();
+            });
+        });
+};
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "id",
+                message: "What is the department's id?"
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is the department's name? (30 Characters)"
+            }
+        ]).then((response) => {
+            const asd = Object.keys(response).filter(k => response[k] !== '').reduce((res, key) => {
+                res[key] = response[key];
+                return res;
+            }, {});
+
+            console.log('AAD', asd)
+
+            connection.query("INSERT INTO department SET ?", asd, (queryErr, queryResponse) => {
+                if (queryErr) {
+                    throw queryErr;
+                }
+                console.log("Success");
+                startMenu();
+            });
+        });
+};
+
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "id",
+                message: "What is the role's id?"
+            },
+            {
+                type: "input",
+                name: "title",
+                message: "What is the title of the role? (30 Characters)"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "What is the role's salary?"
+            },
+            {
+                type: "input",
+                name: "department_id",
+                message: "What is the id of the department this role belongs to?"
+            }
+        ]).then((response) => {
+            const asd = Object.keys(response).filter(k => response[k] !== '').reduce((res, key) => {
+                res[key] = response[key];
+                return res;
+            }, {});
+
+            console.log('AAD', asd)
+
+            connection.query("INSERT INTO role SET ?", asd, (queryErr, queryResponse) => {
+                if (queryErr) {
+                    throw queryErr;
+                }
+                console.log("Success");
+                startMenu();
+            });
+        });
+};
+
+function viewEmployees() {
+    connection.query("SELECT * FROM employee", (queryErr, queryResponse) => {
+
+        const employees = queryResponse;
+        console.table(employees);
+        startMenu();
+    });
+};
+
+function viewDepartments() {
+    connection.query("SELECT * FROM department", (queryErr, queryResponse) => {
+
+        const departments = queryResponse;
+        console.table(departments);
+        startMenu();
+    });
+};
+
+function viewRoles() {
+    connection.query("SELECT * FROM role", (queryErr, queryResponse) => {
+
+        const roles = queryResponse;
+        console.table(roles);
+        startMenu();
+    });
+};
+
+function updateEmployeeRole() {
+
+};
+
+function startMenu() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "initialChoice",
+                choices: [
+                    "Add Employee",
+                    "Add Department",
+                    "Add Role",
+                    "View Employees",
+                    "View Departments",
+                    "View Roles",
+                    "Update Employee Roles",
+                    "Exit Menu"
+                ],
+            }
+        ]).then((response) => {
+            console.log(`You selected to ${response.initialChoice}`);
+
+            // ! Removed .connect() & .end() functions outside of the "Exit Menu" response to fix "Cannot enqueue Query after invoking quit" error.
+            // connection.connect((connectionError) => {
+            // if (connectionError) {
+            //     throw connectionError;
+            // }
+            if (response.initialChoice === "Add Employee") {
                 console.log(1);
 
-                inquirer
-                    .prompt([
-                        {
-                            type: "input",
-                            name: "first_name",
-                            message: "What is the employee's first name? (30 Characters)"
-                        },
-                        {
-                            type: "input",
-                            name: "last_name",
-                            message: "What is the employee's last name? (30 Characters)"
-                        },
-                        {
-                            type: "input",
-                            name: "role_id",
-                            message: "What is the employee's role id?"
-                        },
-                        {
-                            type: "input",
-                            name: "manager_id",
-                            message: "What is the employee's manager's id? Can be left blank if no manager."
-                        }
-                    ]).then((response) => {
-                        const asd = Object.keys(response).filter(k => response[k] !== '').reduce((res, key) => {
-                            res[key] = response[key];
-                            return res;
-                        }, {});
-
-                        console.log('AAD', asd)
-
-                        connection.query("INSERT INTO employee SET ?", asd, (queryErr, queryResponse) => {
-                            if (queryErr) {
-                                throw queryErr;
-                            }
-                            console.log("Success");
-                            connection.end();
-                        });
-                    });
-
+                addEmployee();
             }
             else if (response.initialChoice === "Add Department") {
                 console.log(2);
 
-                inquirer
-                    .prompt([
-                        {
-                            type: "input",
-                            name: "id",
-                            message: "What is the department's id?"
-                        },
-                        {
-                            type: "input",
-                            name: "name",
-                            message: "What is the department's name? (30 Characters)"
-                        }
-                    ]).then((response) => {
-                        const asd = Object.keys(response).filter(k => response[k] !== '').reduce((res, key) => {
-                            res[key] = response[key];
-                            return res;
-                        }, {});
-
-                        console.log('AAD', asd)
-
-                        connection.query("INSERT INTO department SET ?", asd, (queryErr, queryResponse) => {
-                            if (queryErr) {
-                                throw queryErr;
-                            }
-                            console.log("Success");
-                            connection.end();
-                        });
-                    });
-
+                addDepartment();
             }
             else if (response.initialChoice === "Add Role") {
                 console.log(3);
 
-                inquirer
-                    .prompt([
-                        {
-                            type: "input",
-                            name: "id",
-                            message: "What is the role's id?"
-                        },
-                        {
-                            type: "input",
-                            name: "title",
-                            message: "What is the title of the role? (30 Characters)"
-                        },
-                        {
-                            type: "input",
-                            name: "salary",
-                            message: "What is the role's salary?"
-                        },
-                        {
-                            type: "input",
-                            name: "department_id",
-                            message: "What is the id of the department this role belongs to?"
-                        }
-                    ]).then((response) => {
-                        const asd = Object.keys(response).filter(k => response[k] !== '').reduce((res, key) => {
-                            res[key] = response[key];
-                            return res;
-                        }, {});
-
-                        console.log('AAD', asd)
-
-                        connection.query("INSERT INTO role SET ?", asd, (queryErr, queryResponse) => {
-                            if (queryErr) {
-                                throw queryErr;
-                            }
-                            console.log("Success");
-                            connection.end();
-                        });
-                    });
-
+                addRole();
             }
             else if (response.initialChoice === "View Employees") {
                 console.log(4);
 
+                viewEmployees();
             }
             else if (response.initialChoice === "View Departments") {
                 console.log(5);
 
+                viewDepartments();
             }
             else if (response.initialChoice === "View Roles") {
                 console.log(6);
 
+                viewRoles();
             }
             else if (response.initialChoice === "Update Employee Roles") {
                 console.log(7);
 
+                updateEmployeeRole();
             }
-        })
+            else {
+                connection.end();
+            }
+        });
+};
 
-
-    });
+startMenu();
